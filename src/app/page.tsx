55b49393c2ac54bbaf3a60379060064b671d37ac
Mustafa Abdelkawy <32801595+Abdelkawy16@users.noreply.github.com>
2024-03-13 11:42:50 +0200

    add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import Image from "next/image";
+import Header from "@/components/header/header";
 
 export default function Home() {
   return (
-    <>
-    </>
+    <div>
+      {/* Header */}
+      <Header />
+    </div>
   );
 }
